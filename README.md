d2ceee038ad61e863d43ec310218b4312b27ad39
Filip Procházka <filip@prochazka.su>
2013-09-12 16:53:48 +0200

    skeleton

---
+++ README.md
@@ -0,0 +1,15 @@
+Kdyby/Money [![Build Status](https://secure.travis-ci.org/Kdyby/Money.png?branch=master)](http://travis-ci.org/Kdyby/Money)
+===========================
+
+
+Requirements
+------------
+
+Kdyby/Money requires PHP 5.3.2 or higher.
+
+- [Nette Framework 2.0.x](https://github.com/nette/nette)
+
+
+-----
+
+Homepage [http://www.kdyby.org](http://www.kdyby.org) and repository [http://github.com/kdyby/money](http://github.com/kdyby/money).
---
+++ src/Kdyby/Money/exceptions.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008 Filip Procházka (filip@prochazka.su)
+ *
+ * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
+ */
+
+namespace Kdyby\Money;
+
+
+
+/**
+ * @author Filip Procházka <filip@prochazka.su>
+ */
+interface Exception
+{
+
+}
+
+
+
+/**
+ * @author Filip Procházka <filip@prochazka.su>
+ */
+class InvalidStateException extends \RuntimeException implements Exception
+{
+
+}
+
+
+
+/**
+ * @author Filip Procházka <filip@prochazka.su>
+ */
+class InvalidArgumentException extends \InvalidArgumentException implements Exception
+{
+
+}
